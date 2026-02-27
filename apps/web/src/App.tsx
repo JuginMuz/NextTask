db585d1b275ffa42d82248f87c690a12c1eb610f
Jugin <jugin.muzhaqi7@gmail.com>
2026-02-27 15:25:40 +0000

    Added Tailwind and baseline style

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Oki, Tailwind CSS is working!
       </p>
     </>
   )
